4138a381445d706b32059bdf1fc1472e4c4251cd
beginner <me@example.com>
2019-01-13 18:03:40 -0800

    Mailbox

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 // import App from './App';
 
 // import HelloWorld from './component/HelloWorld';
-//import Clock from './component/Clock';
-import LoginControl from './component/LoginControl';
+// import Clock from './component/Clock';
+// import LoginControl from './component/LoginControl';
+import Mailbox from './component/Mailbox';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<LoginControl />, document.getElementById('root'));
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+
+ReactDOM.render(<Mailbox unreadMessages={messages} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
